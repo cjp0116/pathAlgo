ffdbe2f31c3657510511a8b7916135416abfe65b
Jae Pil Cho <48457791+cjp0116@users.noreply.github.com>
2020-01-24 16:53:15 -0800

    Update Main.jsx

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,155 +1,184 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Node from "./Node/Node";
 import NavigationBar from "./Header/NavigationBar";
 import { dijkstra, getNodesInShortestPathOrder } from "./Algorithms/dijkstra";
 import {
-  START_NODE_ROW,
-  START_NODE_COL,
-  FINISH_NODE_ROW,
-  FINISH_NODE_COL,
-  getInitialGrid,
-  getNewGridWithWallToggled
+   getInitialGrid,
+   getNewGridWithWallToggled,
+   clearPath,
+   getStartNode,
+   getFinishNode
+} from "../Shared/actions/gridActions";
 
-} from "../Misc/rowsAndCols";
 import Modal from "../Shared/components/UIElements/Modal";
 import Button from "../Shared/components/UIElements/Button";
-import {djikstraText} from "../Misc/ModalTexts";
+import ModalText, { djikstraText } from "../Misc/ModalTexts";
+import { connect } from "react-redux";
+
 import "./Main.css";
 
+class Main extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         mouseIsPressed: false,
+         route: "home",
+         showModal: true
+      };
+   }
+   componentDidMount() {
+      this.props.getInitialGrid();
+      this.props.getStartNode();
+      this.props.getFinishNode();
+   }
 
-export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      grid: [],
-      mouseIsPressed: false,
-      route : "home",
-      showModal : true
-    };
-  }
+   handleMouseDown(row, col) {
+      const { getNewGridWithWallToggled, grid } = this.props;
+      getNewGridWithWallToggled(grid, row, col);
+      this.setState({ mouseIsPressed: true });
+   }
 
-  componentDidMount() {
-    this.setState({ grid : [] })
-    const grid = getInitialGrid();
-    this.setState({ grid });
-  }
+   handleMouseEnter(row, col) {
+      if (!this.state.mouseIsPressed) return;
+      else this.props.getNewGridWithWallToggled(this.props.grid, row, col);
+   }
 
-  handleMouseDown(row, col) {
-    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-    this.setState({ grid: newGrid, mouseIsPressed: true });
-  }
+   handleMouseUp() {
+      this.setState({ mouseIsPressed: false });
+   }
 
-  handleMouseEnter(row, col) {
-    if (!this.state.mouseIsPressed) return;
-    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-    this.setState({ grid: newGrid });
-  }
-
-  handleMouseUp() {
-    this.setState({ mouseIsPressed: false });
-  }
-
-  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
-    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
-      if (i === visitedNodesInOrder.length) {
-        setTimeout(() => {
-          this.animateShortestPath(nodesInShortestPathOrder);
-        }, 10 * i);
-        return;
+   animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+      for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+         if (i === visitedNodesInOrder.length) {
+            setTimeout(() => {
+               this.animateShortestPath(nodesInShortestPathOrder);
+            }, 10 * i);
+            return;
+         }
+         setTimeout(() => {
+            const node = visitedNodesInOrder[i];
+            document.getElementById(`node-${node.row}-${node.col}`).className =
+               "node node-visited";
+         }, 10 * i);
       }
-      setTimeout(() => {
-        const node = visitedNodesInOrder[i];
-        document.getElementById(`node-${node.row}-${node.col}`).className =
-          "node node-visited";
-      }, 10 * i);
-    }
-  }
+   }
 
-  animateShortestPath(nodesInShortestPathOrder) {
-    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
-      setTimeout(() => {
-        const node = nodesInShortestPathOrder[i];
-        document.getElementById(`node-${node.row}-${node.col}`).className =
-          "node node-shortest-path";
-      }, 50 * i);
-    }
-  }
+   animateShortestPath(nodesInShortestPathOrder) {
+      for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+         setTimeout(() => {
+            const node = nodesInShortestPathOrder[i];
+            document.getElementById(`node-${node.row}-${node.col}`).className =
+               "node node-shortest-path";
+         }, 50 * i);
+      }
+   }
 
-  visualizeDijkstra() {
-    const { grid } = this.state;
-    const startNode = grid[START_NODE_ROW][START_NODE_COL];
-    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
-    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
-    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
-    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
-  }
+   visualizeDijkstra() {
+      const { grid, startNode, finishNode } = this.props;
+      const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+      const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+      this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+   }
 
-  clearPath() {
-    const grid = getInitialGrid();
-    this.setState({ grid : [] });
-    this.setState({ grid : grid})
-  }
-  
-  handleRouteChange = (route) => {
-    this.setState({
-      route : route
-    })
-    console.log(this.state.route)
-  }
+   clearPath() {
+      const grid = getInitialGrid();
+      this.setState({ grid: [] });
+      this.setState({ grid: grid });
+   }
 
-  closeModalHandler() {
-    this.setState({ showModal : false })
-  }
-  
-  render() {
-    const { grid, mouseIsPressed } = this.state;
+   handleRouteChange = route => {
+      this.setState({
+         route: route
+      });
+      console.log(this.state.route);
+   };
 
-    return (
-      <div>
-        <NavigationBar
-          onVisiualizePressed={() => this.visualizeDijkstra()}
-          onClearPathPressed={() => this.clearPath()}
-          onRouteChange={() => this.handleRouteChange()}
-        />
-        <Modal 
-          show={this.state.showModal}
-          onCancel={() => this.closeModalHandler()}
-          header="Djikstra's Algorithm"
-          headerClass="center"
-          footer={<Button danger onClick={() => this.closeModalHandler()}>Close</Button>}
-        >
-          <div className="center">
-            {djikstraText}
-          </div>
-        </Modal>
-        <div className="grid">
-          {grid.map((row, rowIdx) => {
-            return (
-              <div key={rowIdx}>
-                {row.map((node, nodeIdx) => {
-                  const { row, col, isFinish, isStart, isWall } = node;
+   closeModalHandler() {
+      this.setState({ showModal: false });
+   }
+
+   render() {
+      const { mouseIsPressed } = this.state;
+      const { grid } = this.props;
+      return (
+         <div>
+            <NavigationBar
+               onVisiualizePressed={() => this.visualizeDijkstra()}
+               onClearPathPressed={() => this.props.clearPath()}
+               onRouteChange={() => this.handleRouteChange()}
+            />
+            <Modal
+               show={this.state.showModal}
+               onCancel={() => this.closeModalHandler()}
+               header="Djikstra's Algorithm"
+               headerClass="center"
+               footerClass="center"
+               footer={
+                  <React.Fragment>
+                     <Button danger onClick={() => this.closeModalHandler()}>
+                        Close
+                     </Button>
+                     <Button>Next</Button>
+                  </React.Fragment>
+               }
+            >
+               <div className="center"><ModalText djikstraText /></div>
+            </Modal>
+            <div className="grid">
+               {grid.map((row, rowIdx) => {
                   return (
-                    <Node
-                      key={nodeIdx}
-                      col={col}
-                      isFinish={isFinish}
-                      isStart={isStart}
-                      isWall={isWall}
-                      mouseIsPressed={mouseIsPressed}
-                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
-                      onMouseEnter={(row, col) =>
-                        this.handleMouseEnter(row, col)
-                      }
-                      onMouseUp={() => this.handleMouseUp()}
-                      row={row}
-                    ></Node>
+                     <div key={rowIdx}>
+                        {row.map((node, nodeIdx) => {
+                           const { row, col, isFinish, isStart, isWall } = node;
+                           return (
+                              <Node
+                                 key={nodeIdx}
+                                 col={col}
+                                 isFinish={isFinish}
+                                 isStart={isStart}
+                                 isWall={isWall}
+                                 mouseIsPressed={mouseIsPressed}
+                                 onMouseDown={(row, col) =>
+                                    this.handleMouseDown(row, col)
+                                 }
+                                 onMouseEnter={(row, col) =>
+                                    this.handleMouseEnter(row, col)
+                                 }
+                                 onMouseUp={() => this.handleMouseUp()}
+                                 row={row}
+                              ></Node>
+                           );
+                        })}
+                     </div>
                   );
-                })}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
+               })}
+            </div>
+         </div>
+      );
+   }
 }
+
+const mapStateToProps = state => {
+   return {
+      grid: state.grid,
+      START_NODE_ROW: state.START_NODE_ROW,
+      START_NODE_COL: state.START_NODE_COL,
+      FINISH_NODE_COL: state.FINISH_NODE_COL,
+      FINISH_NODE_ROW: state.FINISH_NODE_ROW,
+      startNode: state.startNode,
+      finishNode: state.finishNode
+   };
+};
+
+const mapDispatchToProps = dispatch => {
+   return {
+      getInitialGrid: () => dispatch(getInitialGrid()),
+      getNewGridWithWallToggled: (grid, row, col) =>
+         dispatch(getNewGridWithWallToggled(grid, row, col)),
+      clearPath: () => dispatch(clearPath()),
+      getFinishNode: () => dispatch(getFinishNode()),
+      getStartNode: () => dispatch(getStartNode())
+   };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
